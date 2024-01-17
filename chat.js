0e18d40e1e3f3e912846d4bc7a7247b20d71b641
¨Joaquin <joaquin.brotons@teladochealth.com>
2024-01-17 09:39:53 +0100

    Add sessionId

--- chat.js
+++ chat.js
@@ -1,3 +1,8 @@
+const liveAgentEndpoint = 'https://d.la4-c1cs-ia5.salesforceliveagent.com/chat/rest/'; // Reemplaza con tu endpoint
+const liveAgentVersion = '34'; // La versión de la API de Live Agent
+
+getSessionId();
+
 
 function sendMessage() {
     var message = document.getElementById("userMessage").value;
@@ -13,20 +18,17 @@ function sendMessage() {
     document.getElementById("userMessage").value = "";
 }
 
-function sendMessageSF() {
-    var message = document.getElementById("test").value;
-    var chatBox = document.getElementById("chatBox");
-    
-    var messageDiv = document.createElement("div");
-    messageDiv.classList.add("messageSF");
-    messageDiv.textContent = message;
+function getSessionId() {
+    const url = `${liveAgentEndpoint}/System/SessionId`;
+    const headers = {
+        'X-LIVEAGENT-API-VERSION': liveAgentVersion,
+        'X-LIVEAGENT-AFFINITY': 'null'
+    };
 
-    chatBox.appendChild(messageDiv);
-
-    // Limpiar el campo de entrada después de enviar
-    document.getElementById("test").value = "";
+    fetch(url, { headers: headers })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Resultado de getSessionId:', data); // Agregado console.log
+        })
+        .catch(error => console.error('Error al obtener el ID de sesión:', error));
 }
-
-
-// Inicia el proceso
-//getSessionId();
