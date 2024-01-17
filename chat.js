e61f70c38d178c840d7c5bdd3c72b5d4ec532cc9
¨Joaquin <joaquin.brotons@teladochealth.com>
2024-01-17 12:24:07 +0100

    add

--- chat.js
+++ chat.js
@@ -1,6 +1,9 @@
 const liveAgentEndpoint = 'https://d.la4-c1cs-ia5.salesforceliveagent.com/chat/rest/'; // Reemplaza con tu endpoint
 const liveAgentVersion = '60'; // La versión de la API de Live Agent
-
+sequence = 1;
+affinityToken = null;
+sessionId = null;
+sessionKey = null;
 getSessionId();
 
 
@@ -28,6 +31,9 @@ function getSessionId() {
     fetch(url, { headers: headers, method: 'GET' })
         .then(response => response.json())
         .then(data => {
+            affinityToken = data.affinityToken;
+            sessionId = data.id;
+            sessionKey = data.key;
             console.log('Resultado de getSessionId:', data); // Agregado console.log
             initiateChat(data);
         })
@@ -38,16 +44,16 @@ function initiateChat(sessionData) {
     const chatInitUrl = `${liveAgentEndpoint}Chasitor/ChasitorInit`; // Reemplaza 'hostname' con tu endpoint real
     const headers = {
         'X-LIVEAGENT-API-VERSION': liveAgentVersion, // Reemplaza con tu versión de API
-        'X-LIVEAGENT-AFFINITY': sessionData.affinityToken, // Reemplaza con tu token de afinidad obtenido de la respuesta de SessionId
-        'X-LIVEAGENT-SESSION-KEY': sessionData.key, // Reemplaza con tu clave de sesión obtenida de la respuesta de SessionId
-        'X-LIVEAGENT-SEQUENCE': 1 // Puede empezar desde 1 y aumentar con cada solicitud
+        'X-LIVEAGENT-AFFINITY': affinityToken, // Reemplaza con tu token de afinidad obtenido de la respuesta de SessionId
+        'X-LIVEAGENT-SESSION-KEY': sessionKey, // Reemplaza con tu clave de sesión obtenida de la respuesta de SessionId
+        'X-LIVEAGENT-SEQUENCE': sequence// Puede empezar desde 1 y aumentar con cada solicitud
     };
-
+    
     const chatInitData = {
         organizationId: "00D8J0000008gU0",
         deploymentId: "5728J000000004l",
         buttonId: "5738J000000005A",
-        sessionId: sessionData.id,
+        sessionId: sessionId,
         visitorName: "John Doe",
         prechatDetails: [],
         prechatEntities: [],
@@ -65,6 +71,7 @@ function initiateChat(sessionData) {
         body: JSON.stringify(chatInitData)
     })
     .then(response => {
+        sequence++;
         console.log('Resultado de initiateChat:', response); // Agregado console.log
     })
     .catch(error => console.error('Error al iniciar chat:', error));
